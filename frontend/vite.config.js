4455e90024c58f0756282a3c1c81b8d759b99d02
Jingyi Zhang <jingyizhang111@gmail.com>
2025-11-12 12:59:30 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://trackly-3smc.onrender.com',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // ⬅️ remove /api prefix
-      },
-    },
-  },
 })
+
 
 
