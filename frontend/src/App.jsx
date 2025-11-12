44f7ff1ba6de858d8217a240593ce1b74e73eb89
Jingyi Zhang <jingyizhang111@gmail.com>
2025-11-12 16:32:07 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx"; // ⬅️ this is the import I mentioned
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        {/* add more routes here if you have other pages */}
       </Routes>
     </BrowserRouter>
   );
 }
+
